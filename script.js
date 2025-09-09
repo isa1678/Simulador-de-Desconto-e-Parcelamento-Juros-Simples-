815f483c76920c4f5bb8b88cb8f10b7a50284520
Isabella Cuchara <isabella.cuchara@escola.pr.gov.br>
2025-09-09 11:56:25 +0000

    salvar

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Utilitário para formatar moeda (R$)
+    function moedaBR(valor) {
+        return new Intl.NumberFormat('pt-BR' , { style: 'currency' , currency: `BRL` }).format(valor);
+    }
+    
+    // normaliza entrada (troca vírgula por ponto e converte para número)
+    function toNumber(val) {    // normaliza entrada (troca vírgula por ponto e converte para número)
+        if (typeof val === `number`) return val; 
+        if (!val && val !== 0) return NaN;
+    return parseFloat(String(val).trim,().replace(',')
+    
+

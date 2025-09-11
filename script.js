e648fb02898d70c16821049138e7af81c158f923
Isabella Cuchara <isabella.cuchara@escola.pr.gov.br>
2025-09-11 11:56:41 +0000

    salvar

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Utilitário para formatar moeda (R$)
+    // Utilitário para formatar moeda (R$) 
     function moedaBR(valor) {
-        return new Intl.NumberFormat('pt-BR' , { style: 'currency' , currency: `BRL` }).format(valor);
+        return new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL' }).format(valor);
     }
-    
+
     // normaliza entrada (troca vírgula por ponto e converte para número)
-    function toNumber(val) {    // normaliza entrada (troca vírgula por ponto e converte para número)
-        if (typeof val === `number`) return val; 
+    function toNumber(val) {
+        if (typeof val === 'number') return val; 
         if (!val && val !== 0) return NaN;
-    return parseFloat(String(val).trim,().replace(',')
-    
+        return parse Float(string(val).trim().replace(',','.'));
+    }
+
+    const form = document.getElemetById('form'); 
+    const erro = document. getElementById ('erro');
+    const resultados = document.getElementById('resultados'); 
+    const tabelaSecao = document.getElementById ('tabelaSecao'); 
+
+    const outPrecoComDesconto =
+document.getElementById('precoComDesconto');
+const outValorParcela = document.getElementById('valorParcela');
+const outTotalPagar = document.getElementById('totalPagar');
+const outEconomia = document.getElementById('economia');
+
+
+        
 
